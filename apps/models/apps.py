cfb485fc672d3770298556fec340d15188b09aa6
Ulanbekkn <kubanycbekovulanbek7@gmail.com>
2023-06-15 23:06:38 +0600

    added swagger & jazzmin

--- apps/models/apps.py
+++ apps/models/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BordelConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'models'
+    name = 'apps.models'
--- vip_toktogul/urls.py
+++ vip_toktogul/urls.py
@@ -2,15 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from vip_toktogul.settings import MEDIA_URL, MEDIA_ROOT
+from . import swagger
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/v1/models/', include('models.urls')),
+    path('api/v1/models/', include('apps.models.urls')),
 
-    path('api/v1/users/', include('users.urls'))
+    path('api/v1/users/', include('apps.users.urls'))
 
 ]
 
+urlpatterns += swagger.urlpatterns
 urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
