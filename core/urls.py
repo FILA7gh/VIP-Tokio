221b02d09f6577f76b5bcf28b10d7e028ae307a1
Aftandil <akmatbekovvv@gmail.com>
2023-06-23 17:26:42 +0600

    added jwt urls

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView, TokenObtainPairView
+
 from core.settings.base import MEDIA_URL, MEDIA_ROOT
 from .settings import swagger
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    # models
     path('api/v1/models/', include('apps.models.urls')),
-
+    # others
     path('api/v1/others/', include('apps.others.urls')),
+    # users
+    path('api/v1/users/', include('apps.users.urls')),
 
-    path('api/v1/users/', include('apps.users.urls'))
+    # jwt
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
 ]
 
