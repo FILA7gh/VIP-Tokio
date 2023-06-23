c53a77ed83dc44bce5ffadaa96190e6c6d57e3f6
Aftandil <akmatbekovvv@gmail.com>
2023-06-23 11:45:47 +0600

    added package price

--- apps/others/permissions.py
+++ apps/others/permissions.py
@@ -1,6 +1,4 @@
 from rest_framework.permissions import BasePermission, IsAuthenticated, SAFE_METHODS
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 
 class IsOwnerOrReadOnly(BasePermission):
--- apps/others/views.py
+++ apps/others/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
 from .serializers import *
 from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .permissions import *
-from rest_framework import permissions
 
 
 class SupportAPIView(CreateAPIView):
@@ -26,13 +23,3 @@ class AboutUsAPIView(ListCreateAPIView):
     queryset = AboutUs.objects.all()
     serializer_class = AboutUsSerializer
     permission_classes = [IsAdminOrReadOnly, ]
-
-
-class AboutUsDetailAPIView(ListCreateAPIView):
-    queryset = AboutUs.objects.all()
-    serializer_class = AboutUsSerializer
-    permission_classes = [IsAdminOrReadOnly, ]
-
-
-
-
