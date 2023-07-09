a7bf163d1fe694fcac10cd6c2251b8d91c3eb087
Aftandil <akmatbekovvv@gmail.com>
2023-07-09 22:51:47 +0600

    added validate models

--- apps/models/serializers.py
+++ apps/models/serializers.py
@@ -8,6 +8,7 @@ from apps.services.models import *
 
 # Gallery
 class GallerySerializer(serializers.ModelSerializer):
+    photo = serializers.ImageField(required=True)
 
     class Meta:
         model = ModelsGallery
@@ -18,42 +19,61 @@ class GallerySerializer(serializers.ModelSerializer):
 
 
 class PackagePriceSerializer(serializers.ModelSerializer):
+    apartments_1h = serializers.IntegerField(max_value=1000000)
+    apartments_2h = serializers.IntegerField(max_value=1000000)
+    apartments_night = serializers.IntegerField(max_value=1000000)
+    departure_1h = serializers.IntegerField(max_value=1000000)
+    departure_2h = serializers.IntegerField(max_value=1000000)
+    departure_night = serializers.IntegerField(max_value=1000000)
+
     class Meta:
         model = PackagePrice
         fields = '__all__'
 
 
 class BasicServiceSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=255)
+
     class Meta:
         model = BasicService
         fields = ['title']
 
 
 class AdditionalServiceSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=255)
+
     class Meta:
         model = AdditionalService
         fields = ['title']
 
 
 class MassageSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=255)
+
     class Meta:
         model = Massage
         fields = ['title']
 
 
 class ExtremeSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=255)
+
     class Meta:
         model = Extreme
         fields = ['title']
 
 
 class SadoMazoSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=255)
+
     class Meta:
         model = SadoMazo
         fields = ['title']
 
 
 class StripteaseSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=255)
+
     class Meta:
         model = Striptease
         fields = ['title']
@@ -65,7 +85,7 @@ class StripteaseSerializer(serializers.ModelSerializer):
 class ModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Model
-        fields = 'nickname height weight breast phone_number'.split()
+        fields = ['nickname', 'height', 'weight', 'breast', 'phone_number']
 
 
 class ModelDetailSerializer(serializers.ModelSerializer):
@@ -80,12 +100,36 @@ class ModelDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Model
-        fields = 'gallery nickname phone_number age height weight appearance eyes hairs ' \
-                 'breast description speak_english type package_price area schedule basic_service ' \
-                 'additional_service massage striptease sadomazo extreme is_trans in_osh'.split()
+        fields = ['gallery', 'nickname', 'phone_number', 'age', 'height', 'weight',
+                  'appearance', 'eyes', 'hairs', 'breast', 'description', 'speak_english',
+                  'type', 'package_price', 'area', 'schedule', 'basic_service', 'additional_service',
+                  'massage', 'striptease', 'sadomazo', 'extreme', 'is_trans in_osh']
 
 
 class ModelValidateSerializer(serializers.Serializer):
+    nickname = serializers.CharField(max_length=100)
+    description = serializers.CharField()
+    age = serializers.IntegerField()
+    appearance = serializers.ListField(child=serializers.CharField())
+    height = serializers.IntegerField()
+    weight = serializers.IntegerField()
+    eyes = serializers.ListField(child=serializers.CharField())
+    hairs = serializers.ListField(child=serializers.CharField())
+    type = serializers.ListField(child=serializers.CharField())
+    area = serializers.CharField(max_length=100)
+    breast = serializers.ListField(child=serializers.CharField())
+    phone_number = serializers.CharField(max_length=16)
+    schedule = serializers.CharField(max_length=19)
+    speak_english = serializers.BooleanField()
+    is_trans = serializers.BooleanField()
+    in_osh = serializers.BooleanField()
+    package_price = serializers.ListField()
+    basic_service = serializers.PrimaryKeyRelatedField(many=True)
+    additional_service = serializers.PrimaryKeyRelatedField(many=True)
+    massage = serializers.PrimaryKeyRelatedField(many=True)
+    extreme = serializers.PrimaryKeyRelatedField(many=True)
+    sadomazo = serializers.PrimaryKeyRelatedField(many=True)
+    striptease = serializers.PrimaryKeyRelatedField(many=True)
 
     @staticmethod
     def validate_phone_number(phone_number):
